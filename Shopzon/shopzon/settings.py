922648af221faa1cb03c26b62701f8d9adf43282
progIfe <ifeoluwasamson90@gmail.com>
2023-10-21 23:30:56 +0100

    Changed database to PostgreSQL and migrated

--- Shopzon/shopzon/settings.py
+++ Shopzon/shopzon/settings.py
@@ -136,10 +136,22 @@ WSGI_APPLICATION = 'shopzon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'mPd7PoVudZMWHLow1eMK',
+        'HOST': 'containers-us-west-73.railway.app',
+        'PORT': '7065',
     }
 }
 
